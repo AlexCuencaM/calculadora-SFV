fd77683087d14f6797d7f300314112c2784343ee
JerryRivera1994 <jerry_rivera1994@hotmail.com>
2020-02-09 03:48:29 -0500

    Metodo de funcion eliminar ok

--- resources/js/interfazCalcular.js
+++ resources/js/interfazCalcular.js
@@ -18,48 +18,57 @@ document.getElementById("consumo").addEventListener("click",function(){
 
 
 
-//declaracion de variables
+//Método que llama a la función datos tabla para su posterior  inserción
 function anadir(){
-		var electrodomestico={tipo:"televisor",tiempo:"70 W",cantidad:"100"}; //datos de prueba
-		var nombreTabla=document.getElementById("tablaConsumo");//nombre de la tabla html
+		//electrodomestico de prueba
+		var electrodomestico={tipo:"televisor",tiempo:"70 W",cantidad:"100"}; 
+
+		//nombre de la tabla html
+		var nombreTabla=document.getElementById("tablaConsumo");
+
+		//llama al método datosTabla la cual inserta los datos en la tabla
 		datosTabla(electrodomestico,nombreTabla);
 
 }
 
 
-//insertar datos a tabla
+//Método que inserta los datos en la tabla
 function datosTabla(electrodomestico,nombreTabla){		
+
+		//se añade una fila a la tabla
 		var fila=nombreTabla.insertRow(0+1);
+
+		// le asigna al objeto cell 3
 		var cell=[fila.insertCell(0),fila.insertCell(1),fila.insertCell(2),fila.insertCell(3)]
-		cell[0].innerHTML='<td> '+electrodomestico.tipo+'<input type="hidden" name="producto[]" value="Producto"></td>';
+
+		//inserta en cada celda los atributos del objeto
+		cell[0].innerHTML='<td> '+electrodomestico.tipo+'<input type="hidden" name="producto[]" value="Producto" id="tv"></td>';
 		cell[1].innerHTML='<td><input type="text" value="'+electrodomestico.tiempo+'" name="tiempo[]"></td>';
 		cell[2].innerHTML='<td><input type="text" value="'+electrodomestico.cantidad+'" name="cantidad[]"></td>';
 		cell[3].innerHTML='<td><input type="button" value="Eliminar" name="eliminar[]"  id="eliminar" onclick="eliminarFila()"></td>';
 }
 
 
-//Eiminar
-//
+//Método de eliminar fila de la tabla
 
-/*function eliminarFila(){
-	alert("hola");
-	//$(this).parent().parent().fadeOut("slow",function(){$(this).remove});
-	var pre=getRowSelected(this);
-	console.log(pre);
+function eliminarFila(){
+	var fila_obt=getRowSelected();
+
+	//Elimina el objecto la cual contiene toda la fila
+	fila_obt.remove();
 
 }
-*/
 
-/*
-function getRowSelected(objectPressed){
-	var a=objectPressed.parentNode.parentNode;
-	var electrodomestico=a.getElementsByTagName("td")[0].getElementsByTagName("input")[0].innerHTML;
-	var cantidad=a.getElementsByTagName("td")[1].getElementsByTagName("input")[0].innerHTML;
-	var tiempo=a.getElementsByTagName("td")[2].getElementsByTagName("input")[0].innerHTML;
-	var electrodomesticos=[electrodomestico,cantidad,tiempo];
-	return electrodomesticos;
 
-}*/
+//Método de obtener fila de la tabla
+
+function getRowSelected(){
+
+	//Obtiene toda la fila 
+	var obtener_fila=	document.getElementById("eliminar").parentNode.parentNode;
+
+	return obtener_fila;
+}
 
 
 
