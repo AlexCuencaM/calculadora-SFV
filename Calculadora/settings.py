cb3ac70ac0a289327949b1052b9283544c93ebe0
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-02-20 11:37:03 -0500

    Por punto

--- Calculadora/settings.py
+++ Calculadora/settings.py
@@ -157,6 +157,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
