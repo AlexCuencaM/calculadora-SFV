a7fe4e09b3de5b17c4c54bdf238ba6ae3693a171
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-02-25 08:11:44 -0500

    Debugging wsgi

--- Calculadora/wsgi.py
+++ Calculadora/wsgi.py
@@ -14,4 +14,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Calculadora.settings.production')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
