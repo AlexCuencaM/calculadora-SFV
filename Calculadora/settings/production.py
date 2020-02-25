60279c6f0ffd888b6e52a048762db05255409de9
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-02-25 08:32:28 -0500

    environ

--- Calculadora/settings/production.py
+++ Calculadora/settings/production.py
@@ -7,11 +7,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os,django_heroku,dj_database_url
+import os,django_heroku,dj_database_url,environ
 from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
