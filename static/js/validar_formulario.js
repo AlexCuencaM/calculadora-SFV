28bcc2e3649e0198fd0223121d8e89160fbe20f3
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-09-14 19:21:08 -0500

    Adicion de popovers

--- static/js/validar_formulario.js
+++ static/js/validar_formulario.js
@@ -1,4 +1,3 @@
-
 function validar(e){	
 	const array=datosformulario();		
 	const validar = array.every(value => value !== "");		
@@ -7,6 +6,29 @@ function validar(e){
 		alert("Todos los campos son obligatorios");
 	}	
 }
+const descripcion = () =>{
+	const resumen = "Soporta un perfil de carga";
+	const otroResumen = "hasta una carga pico de";
+	const resumenBattery = "Tiene una energía acumulada de";
+	const consejo= "por lo que este banco son las necesarias para un sistema de 48V"
+	return{
+		bateria:{	
+			id: "info-bateria",		
+			descripcion:[`${resumenBattery} 1500Wh`,`${resumenBattery} 3000Wh`,
+			`${resumenBattery} 6000Wh, ${consejo}`]
+		},
+
+		inversor:{	
+			id: "info-inversor",		
+			descripcion:[
+				`${resumen} pico de hasta 3kW`,
+				`${resumen} mínimo de 3kW ${otroResumen} 5 kW.`,
+				`${resumen} de 5.5 kW ${otroResumen} 10 kW`,
+			]
+		},
+	}
+}
+
 async function mostrarFormulario(url,id) {				
 	fetch(url, initParams())
 		.then(response => response.text())		
@@ -18,10 +40,29 @@ async function mostrarFormulario(url,id) {
 
 function form(){			
 	const form = document.querySelector('#impl');	
+	const objeto = descripcion();
 	form.addEventListener('submit',validar);
 	buttons();
-}
 
+	$('[data-toggle="popover"]').popover();
+	initPopover(objeto.inversor,"inversor");
+	initPopover(objeto.bateria,"voltaje");
+}
+function initPopover(objeto,nombre){	
+	const info = document.querySelector(`#${objeto.id}`);
+	const select = document.getElementById(`${nombre}`);
+	let title = select.value;
+	select.addEventListener("change",()=>{
+		if(nombre ==="voltaje")
+		{
+			title = `Batería de ${select.value}V 100Ah`
+			if(select.value==48)
+				title = `Batería de ${select.value}V 400Ah`
+		}
+		$(`#${objeto.id}`).attr("data-original-title",title );
+		$(`#${objeto.id}`).attr("data-content",objeto.descripcion[select.selectedIndex]);		
+	});	
+}
 function buttons(){
 	const buttons = document.getElementsByClassName("preven");	
 	for(let i=0; i<buttons.length; i++)
